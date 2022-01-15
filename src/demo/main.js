c1866c9b60214a2c6567f256ba0f5437bbf02a2d
lizicong <1101258845@qq.com>
2022-01-15 15:31:34 +0800

    集成ant

--- src/demo/main.js
+++ src/demo/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '../packages/web/ant';
 
 new Vue({
     render: h => h(App)
---
+++ src/packages/web/ant.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import 'ant-design-vue/dist/antd.css';
+
+import { Button, Row, Col } from 'ant-design-vue';
+
+Vue.use(Button);
+Vue.use(Row);
+Vue.use(Col);
