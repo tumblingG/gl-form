2b6bf9a7ed22d884b6ee3805a143963f6af20d81
lizicong <1101258845@qq.com>
2022-01-15 14:05:03 +0800

    初始化demo

---
+++ src/demo/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <test/>
+  </div>
+</template>
+<script>
+  import Test from './test/test.vue';
+  export default {
+    name: 'App',
+    components: { Test },
+
+    data () {
+        return {message: '6666'}
+    }
+  }
+</script>
+
+<style lang="less">
+  #app {
+    color: red;
+  }
+</style>
---
+++ src/demo/test/test.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import 'reflect-metadata';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+    name: 'Test'
+})
+export default class Test extends Vue {
+
+}
+</script>
+
+<template src="./test.html"></template>
+<style src="./test.less" lang="less" scoped></style>
